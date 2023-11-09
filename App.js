71838fff49d8097cd5baa0ece54057f87dd266fb
Radoslav Hajšo <radoslavhajso@gmail.com>
2023-11-09 12:28:40 +0100

    Založenie homescreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeSreen from './screens/HomeSreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HomeSreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/HomeSreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeSreen = () => {
+  return (
+    <View>
+      <Text>HomeSreen</Text>
+    </View>
+  )
+}
+
+export default HomeSreen
+
+const styles = StyleSheet.create({})
